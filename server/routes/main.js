4e807e7fc47d6952ae78617d95b7cb8f9eea8a88
Juan Francisco <juanfranciscofernandezherreros@gmail.com>
2025-01-18 01:30:18 +0100

    How to Build a Blog with Node.js, Express & MongoDB - PART 1

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 // Routes
-router.get('',(req,res)=> {
-    res.send("Hello World");
+router.get('', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Hello World</title>
+            <link rel="stylesheet" href="/css/styles.css">
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+        </html>
+    `);
 });
 
 module.exports = router;
