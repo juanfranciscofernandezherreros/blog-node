cbb601853dbbb8c0646934b61471a749e69bd613
Juan Francisco <juanfranciscofernandezherreros@gmail.com>
2025-01-21 23:46:49 +0100

    How to Build a Blog with Node.js, Express & MongoDB - PART 6 (View post)

--- server/routes/main.js
+++ server/routes/main.js
@@ -41,6 +41,29 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+  
+      const data = await Post.findById({ _id: slug });
+  
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+  
+
 function insertPostData() {
     Post.insertMany([
         {
