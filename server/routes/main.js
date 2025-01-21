7a4ca4f41d232f481203ebd75b17be594daea5a2
Juan Francisco <juanfranciscofernandezherreros@gmail.com>
2025-01-21 23:37:05 +0100

    How to Build a Blog with Node.js, Express & MongoDB - PART 5 (Adding pagination))

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,13 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    const locals = {
+router.get('', async (req, res) => {
+    try {
+      const locals = {
         title: "NodeJs Blog",
         description: "Simple Blog created with NodeJs, Express & MongoDb."
-    };
-    res.render('index', { locals });
-});
+      }
+  
+      let perPage = 5;
+      let page = req.query.page || 1;
+  
+      const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+  
+      // Count is deprecated - please use countDocuments
+      // const count = await Post.count();
+      const count = await Post.countDocuments({});
+      const nextPage = parseInt(page) + 1;
+      const hasNextPage = nextPage <= Math.ceil(count / perPage);
+  
+      res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+      });
+  
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
 
 router.get('/about', (req, res) => {
     res.render('about');
