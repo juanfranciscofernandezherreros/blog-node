861a9e85b0615c5607770be7a5b7aac9b4d78a3e
Juan Francisco <juanfranciscofernandezherreros@gmail.com>
2025-01-25 21:24:53 +0100

    How to Build a Blog with Node.js, Express & MongoDB - PART 8 (Register & Login with bcrypt)

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const connectDB = require('./server/config/db');
 const mainRouter = require('./server/routes/main'); // Asegúrate de que esta ruta sea correcta
-
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +14,18 @@ connectDB();
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }));
+  
 
 // Configuración del motor de plantillas
 app.use(expressLayout);
