ff46effe0ecd65c0d99eee3a09947ec80fc4ce57
Juan Francisco <juanfranciscofernandezherreros@gmail.com>
2025-01-25 22:16:07 +0100

    How to Build a Blog with Node.js, Express & MongoDB - PART 10

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
+require('dotenv').config();
+
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
-const connectDB = require('./server/config/db');
-const mainRouter = require('./server/routes/main'); // Asegúrate de que esta ruta sea correcta
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Conexión a la base de datos
+  
+// Connect to DB
 connectDB();
 
-// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI
-    }),
-    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
-  }));
-  
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 
-// Configuración del motor de plantillas
+app.use(express.static('public'));
+
+// Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-// Archivos estáticos
-app.use(express.static('public'));
 
-// Rutas
+app.locals.isActiveRoute = isActiveRoute; 
+
+
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
-// Arranque del servidor
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, ()=> {
+  console.log(`App listening on port ${PORT}`);
 });
