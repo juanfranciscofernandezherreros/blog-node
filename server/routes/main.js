b5be313316d6ab7efdbc5aaaf436b63b340ddfdb
unknown <juanfranciscofernandezherreros@gmail.com>
2025-03-25 00:26:46 +0100

    dashboard

--- server/routes/main.js
+++ server/routes/main.js
@@ -207,6 +207,7 @@ router.get('/', async (req, res) => {
         $project: {
           title: 1,
           summary: 1, // ✅ Incluimos el resumen del post
+          images: 1, // ✅ Aquí se incluye el campo
           publishDate: 1,
           "category.name": 1,
           "author.username": 1,
